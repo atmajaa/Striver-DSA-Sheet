982ad2a048a64ddf91a3ac772cf32dc6ec79df62
aatmaja <aatmajapitale@gmail.com>
2024-09-27 00:53:38 +0530

    27th Sept

---
+++ Arrays/sortZerosOnesAndTwos.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+public class sortZerosOnesAndTwos {
+    public void sortColors(int[] nums) {
+        int zeros = 0;
+        int ones = 0;
+        int twos = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == 0){
+                zeros++;
+            }
+            if(nums[i] ==1){
+                ones++;
+            }
+            if(nums[i] == 2){
+                twos++;
+            }
+        }
+        for(int i=0; i<zeros; i++){
+            nums[i] = 0;
+        }
+        for(int i=zeros; i<zeros+ones; i++){
+            nums[i] = 1;
+        }
+        for(int i=zeros+ones; i<nums.length;i++){
+            nums[i] = 2;
+        }
+    }
+}
