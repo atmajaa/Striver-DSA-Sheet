465d1fc4a4219615c7330f8b974fa8cf023be1e2
aatmaja <aatmajapitale@gmail.com>
2024-09-28 01:50:17 +0530

    28th Sept

---
+++ Arrays/KadanesAlgorithm.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class KadanesAlgorithm {
+    int maxSubArray(int arr[]){
+        int max_so_far = arr[0];
+        int current_max = arr[0];
+        for(int i=1; i<arr.length; i++){
+            current_max = Math.max(arr[i], current_max+arr[i]);
+            max_so_far = Math.min(current_max, max_so_far);
+
+        }
+    return max_so_far;
+    }
+    public static void main(String[] args) {
+        KadanesAlgorithm kadanesAlgorithm = new KadanesAlgorithm();
+        int arr[] = {-2,3,4,-5,9,-5};
+        System.out.println(kadanesAlgorithm.maxSubArray(arr));
+    }
+}
